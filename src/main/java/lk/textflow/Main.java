4ca61094de67b581fcf30cf6c16927b661e24088
Rajaka Udara <rajakaudr@gmail.com>
2026-09-04 23:02:52 +0530

    Set up shared TextFlow database foundation

--- src/main/java/lk/textflow/Main.java
+++ src/main/java/lk/textflow/Main.java
@@ -1,7 +1,6 @@
 package lk.textflow;
 
 import lk.textflow.config.DatabaseConnection;
-
 import java.sql.Connection;
 
 public class Main {
@@ -11,20 +10,11 @@ public class Main {
         try (Connection connection =
                      DatabaseConnection.getConnection()) {
 
-            if (connection != null &&
-                    !connection.isClosed()) {
-
-                System.out.println(
-                        "TextFlow database connected successfully!"
-                );
-            }
-
-        } catch (Exception e) {
-
             System.out.println(
-                    "Database connection failed!"
+                    "TextFlow database connected successfully!"
             );
 
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/textflow/Main.java
+++
@@ -1,7 +0,0 @@
-package lk.textflow;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("TextFlow project is running!");
-    }
-}
